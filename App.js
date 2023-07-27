5667ac99d2b70b54dc972fe0ad5e9cd923fb52a3
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-27 16:08:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import OrderProvider from './Store/OrderProvider';
+import CandyForm from './Components/Candy/CandyForm';
+import CandyList from './Components/Candy/CandyList';
+import Header from './Components/Header/Header';
+import Cart from './Components/Cart/Cart';
+
+function App() {
+
+  const [ShowCart,setShowCart]=useState(false)
+
+  const ShowCartHandler=()=>{
+    setShowCart(true)
+  }
+
+  const CancelCartHandler=()=>{
+    setShowCart(false)
+  }
+  
+
+  return(
+    <OrderProvider>
+     <header>
+         <Header onDisplay={ShowCartHandler} />
+     </header>
+     <main>
+        <CandyForm />
+        <CandyList />
+     </main>
+     {ShowCart && <Cart onCancel={CancelCartHandler} />}
+    </OrderProvider>
+  )
+  
+}
+
+export default App;
