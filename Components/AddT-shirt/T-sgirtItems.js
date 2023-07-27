f4f5ea815ad12ef612760994ff2bdb7384ba6727
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-27 17:13:46 +0530

    Update T-sgirtItems.js

--- Components/AddT-shirt/T-sgirtItems.js
+++ Components/AddT-shirt/T-sgirtItems.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React,{useContext} from "react";
+
 
 
 const CandyItems=(props)=>{
 
+    
 
     return(
         <React.Fragment>
             <div>
                 {props.name}-{props.description}-Rs{props.price}
             </div>
-            <button onClick={props.onAddL}>Buy L</button>
-            <button onClick={props.onAddM}>Buy M</button>
-            <button onClick={props.onAddS}>Buy S</button>
+            <button onClick={props.onAddL}>Buy L {(props.NumberL)} </button>
+            <button onClick={props.onAddM}>Buy M {(props.NumberM)} </button>
+            <button onClick={props.onAddS}>Buy S {(props.NumberS)} </button>
         </React.Fragment>
     )
 }
