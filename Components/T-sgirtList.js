663fb77e42e2995e883452708aebb8cbf8716f30
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-27 16:10:21 +0530

    Create T-shirtList.js

---
+++ Components/T-sgirtList.js
@@ -0,0 +1,42 @@
+import React,{useContext} from "react";
+import OrderContext from "../../Store/order-context";
+import CandyItems from "./CandyItems";
+
+const CandyList=()=>{
+
+    const CandyCtx=useContext(OrderContext)
+
+    const AddLHandler=(id)=>{
+        CandyCtx.AddbyOne(id)
+    }
+
+    const AddMHandler=(id)=>{
+        CandyCtx.AddbyTwo(id)
+    }
+
+    const AddSHandler=(id)=>{
+        CandyCtx.AddbyThree(id)
+    }
+
+    const Candylist=<ul>{CandyCtx.candy.map(Candy=>
+                           <CandyItems 
+                               key={Candy.id}
+                               name={Candy.name}
+                               description={Candy.description}
+                               price= {Candy.price} 
+                               onAddL={AddLHandler.bind(null,Candy.id)} 
+                               onAddM={AddMHandler.bind(null,Candy.id)} 
+                               onAddS={AddSHandler.bind(null,Candy.id)} 
+                            />
+                           
+                    )} 
+    </ul>
+
+    return (
+        <React.Fragment>
+            {Candylist}
+        </React.Fragment>
+    )
+}
+
+export default CandyList
